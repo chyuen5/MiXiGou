588479b4fdbe00b1777a4ddeb9f965e12a26a320
chyuen5 <chyuen5@outlook.com>
2018-08-06 22:56:14 +0800

    加入人脸识别，添加文件，还不能运行

--- app/src/main/java/com/hsinfo/mixigou/App.java
+++ app/src/main/java/com/hsinfo/mixigou/App.java
@@ -7,6 +7,14 @@ import android.content.SharedPreferences;
 import android.widget.RemoteViews;
 import android.widget.Toast;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
+import android.net.Uri;
+import android.util.Log;
+
+import com.hsinfo.mixigou.face.FaceDB;
 import com.umeng.commonsdk.UMConfigure;
 import com.umeng.message.IUmengRegisterCallback;
 import com.umeng.message.PushAgent;
@@ -16,6 +24,11 @@ import com.umeng.message.entity.UMessage;
 
 public class App extends Application
 {
+    private final String TAG = this.getClass().toString();
+    public FaceDB mFaceDB;
+
+    Uri mImage;
+
     private static String m_str_deviceToken;
 
     private static Context mContext;
@@ -29,6 +42,9 @@ public class App extends Application
         mContext = getApplicationContext();
 
         sp = getSharedPreferences("userdata", 0);
+
+        mFaceDB = new FaceDB(this.getExternalCacheDir().getPath());
+        mImage = null;
 
         //UMConfigure.init(this, UMConfigure.DEVICE_TYPE_PHONE, "1fe6a20054bcef865eeb0991ee84525b");
         UMConfigure.init(this, "5adb001ff29d981f21000059", "Umeng", UMConfigure.DEVICE_TYPE_PHONE, "5152f5b388deeeac6e824a6583a414e1");
@@ -129,6 +145,53 @@ public class App extends Application
         };
         //使用自定义的NotificationHandler
         mPushAgent.setNotificationClickHandler(notificationClickHandler);
+    }
+
+    public void setCaptureImage(Uri uri) {
+        mImage = uri;
+    }
+
+    public Uri getCaptureImage() {
+        return mImage;
+    }
+
+    /**
+     * @param path
+     * @return
+     */
+    public static Bitmap decodeImage(String path) {
+        Bitmap res;
+        try {
+            ExifInterface exif = new ExifInterface(path);
+            int orientation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL);
+
+            BitmapFactory.Options op = new BitmapFactory.Options();
+            op.inSampleSize = 1;
+            op.inJustDecodeBounds = false;
+            //op.inMutable = true;
+            res = BitmapFactory.decodeFile(path, op);
+            //rotate and scale.
+            Matrix matrix = new Matrix();
+
+            if (orientation == ExifInterface.ORIENTATION_ROTATE_90) {
+                matrix.postRotate(90);
+            } else if (orientation == ExifInterface.ORIENTATION_ROTATE_180) {
+                matrix.postRotate(180);
+            } else if (orientation == ExifInterface.ORIENTATION_ROTATE_270) {
+                matrix.postRotate(270);
+            }
+
+            Bitmap temp = Bitmap.createBitmap(res, 0, 0, res.getWidth(), res.getHeight(), matrix, true);
+            Log.d("com.arcsoft", "check target Image:" + temp.getWidth() + "X" + temp.getHeight());
+
+            if (!temp.equals(res)) {
+                res.recycle();
+            }
+            return temp;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
     public static Context getInstance() {
